25accfcc74224281aa6392dd8a4c6b6772ac089b
Runatorium <stefano.alesci1@gmail.com>
2023-05-30 16:55:01 +0200

    Api working, working on passing response.data on ProjectCard with Props

---
+++ src/components/AppMain.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export default{
+    name: "AppMain",
+    data(){
+        return{
+            postsData: []
+        }
+    },
+
+    methods: {
+        getPosts(){
+            axios.get('http://localhost:8000/api/posts')
+            .then(response =>{
+                this.postsData = response.data.results;
+                console.log(this.postsData);
+            })
+        }
+    },
+    mounted(){
+        this.getPosts();
+    } ,
+
+}
+
+</script>
+
+<template>
+
+</template>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<script>
+ export default{
+    name: "ProjectCard"
+ }
+</script>
+
+
+<template>
+<div class="card" >
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+</template>
+
+<style lang="css" scoped> 
+    .card{
+        width: 18rem;
+    }
+</style>
+
