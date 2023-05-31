694a0588e9bb85185dfdcff898805ff15358e4c1
Runatorium <stefano.alesci1@gmail.com>
2023-05-31 16:46:58 +0200

    Re-route  from navBar to components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script>
 
 import ProjectList from './pages/ProjectList.vue';
+import NavBar from './components/NavBar.vue';
 
 export default{
-    components: { ProjectList }
+    components: { ProjectList, NavBar }
 }
 </script>
 
+
 <template>
+  <header>
+      <NavBar></NavBar>
+  </header>
   <RouterView></RouterView>
 </template>
 
 <style lang="scss">
-@use './styles/general.sass'
+@use './styles/general.sass';
 
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,2 +1,35 @@
 <script>
+import { router } from '../router.js';
+
+export default{
+    name: "NavBar",
+}
 </script>
+
+
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Boolfolio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link :to="{name:'Home'}" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{name:'Contact'}" class="nav-link">Contact</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{name:'Project'}" class="nav-link">Projects</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{name:'About'}" class="nav-link">Home</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
